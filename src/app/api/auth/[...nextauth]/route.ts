817a8f9a2dd99fe576270704f34c810139ec7362
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-04-23 23:41:41 -0300

    feat: 🎸 add next-auth setup

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,17 @@
+import { db } from '@/lib/prisma';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import { Adapter } from 'next-auth/adapters';
+import GoogleProvider from 'next-auth/providers/google';
 
 const handler = NextAuth({
-  providers: []
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    })
+  ]
 });
 
 export { handler as GET, handler as POST };
