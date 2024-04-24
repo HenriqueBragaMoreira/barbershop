b7e4add76495b23e2cb0b840044542d765d4ead9
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-04-23 23:45:55 -0300

    feat: 🎸 add AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import { Footer } from '@/components/footer';
+import { AuthProvider } from '@/providers/auth';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../styles/globals.css';
-import { Footer } from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} dark`}>
-        {children}
+        <AuthProvider>{children}</AuthProvider>
         <Footer />
       </body>
     </html>
